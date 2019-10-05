a6560ee7d8eeb8aec7c2e6bb7735c9d3a7f01da2
gimn2hstand <gimn2hstand@mail.ru>
2019-10-05 21:31:20 +0300

    Более удобный способ повесить скролл на главу

--- js/topic.js
+++ js/topic.js
@@ -1,4 +1,20 @@
+/**
+ * October 5, 2019
+ * Copyright, Mikhail K., 2019
+ */
+
+window.onload = function() {
+    console.log('test');
+    let links = document.querySelectorAll('.table_of_contents ul li a');
+    links.forEach(function (link) {
+        function goFunction() {
+            goTo(link.dataset.section);
+        }
+        link.addEventListener('click', goFunction);
+    });
+};
+
 function goTo(section) {
-    document.getElementById(section).scrollIntoView({behavior:'smooth'});
+    document.getElementById(section).scrollIntoView({behavior: 'smooth'});
     //location.href = '#' + section;
 }
