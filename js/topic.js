b3442790171b958d94d66a6e23e58573deba82d2
gimn2hstand <gimn2hstand@mail.ru>
2019-10-06 08:59:18 +0300

    Динамическое добавление содержания в меню-гамбургер

--- js/topic.js
+++ js/topic.js
@@ -7,9 +7,12 @@ window.onload = function() {
     let chapters = document.querySelectorAll('.content .chapter');
     let tableOfContents = document.getElementById('table_of_contents');
     let tableOfContentsHTML = '<b>Содержание:</b><ul>';
+    let tableOfContentsNavigation = document.getElementById('navigation-table_of_contents');
+    let tableOfContentsNavigationHTML = 'Содержание:';
     chapters.forEach(function (chapter) {
         chapter.innerHTML = '<h4>' + chapter.dataset.chapter + '</h4>' + chapter.innerHTML;
         tableOfContentsHTML += '<li><a data-section="' + chapter.id + '">' + chapter.dataset.chapter + '</a></li>';
+        tableOfContentsNavigationHTML += '<a class="mdl-navigation__link" data-section="' + chapter.id + '">' + chapter.dataset.chapter + '</a>';
         let illustrations = document.querySelectorAll('.content #' + chapter.id + " img");
         let num = 1;
         illustrations.forEach(function (illustration) {
@@ -22,15 +25,20 @@ window.onload = function() {
         })
     });
     tableOfContents.innerHTML = (tableOfContentsHTML += '</ul>');
+    tableOfContentsNavigation.innerHTML = tableOfContentsNavigationHTML;
 
-    let links = document.querySelectorAll('#table_of_contents ul li a');
+    goClickListener(document.querySelectorAll('#table_of_contents ul li a'));
+    goClickListener(document.querySelectorAll('#navigation-table_of_contents a'));
+};
+
+function goClickListener(links) {
     links.forEach(function (link) {
         function goFunction() {
             goTo(link.dataset.section);
         }
         link.addEventListener('click', goFunction);
     });
-};
+}
 
 function goTo(section) {
     document.getElementById(section).scrollIntoView({behavior: 'smooth'});
